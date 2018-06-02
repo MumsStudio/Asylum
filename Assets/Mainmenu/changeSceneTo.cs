9dbfc6a8e5b50eae6dae3bb1a5c16a32e99d6269
madhotdog <jaymjj828@gmail.com>
2018-06-01 21:18:59 -0400

    jinjin update

---
+++ Assets/Mainmenu/changeSceneTo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeSceneTo : MonoBehaviour {
+
+	public void play(string sceneTochangeTo){
+		Application.LoadLevel (sceneTochangeTo);
+	}
+
+	public void quit(){
+		Debug.Log ("Quit !!!!");
+		Application.Quit ();
+	}
+}
---
+++ Assets/TheGame/dialogHolder.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class dialogHolder : MonoBehaviour
+{
+
+    public string dialogue;
+    private DMM dMAn;
+
+    public string[] dialogueLines;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        dMAn = FindObjectOfType<DMM>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                //dMAn.ShowBox(dialogue);
+
+                if (!dMAn.dialogActive)
+                {
+                    dMAn.dialogLines = dialogueLines;
+                    dMAn.currentLine = 0;
+                    dMAn.ShowDialogue();
+                }
+            }
+        }
+    }
+}
