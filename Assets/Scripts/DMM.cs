cbf3157fc3843c60aa129c5ed9444055ca02dd06
Unknown <urehelonn@gmail.com>
2018-06-01 21:27:31 -0400

    Folder category modified

---
+++ Assets/Scripts/DMM.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine.UI;
+using System.Collections;
+using UnityEngine;
+
+public class DMM : MonoBehaviour {
+
+    public GameObject dBox;
+    public Text dText;
+
+    public bool dialogActive;
+
+    public string[]dialogLines;
+    public int currentLine;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (dialogActive && Input.GetKeyDown(KeyCode.Space))
+        {
+            //dBox.SetActive(false);
+            //dialogActive = false;
+
+            currentLine++;
+        }
+        if (currentLine >= dialogLines.Length)
+        {
+            dBox.SetActive(false);
+            dialogActive = false;
+
+            currentLine = 0;
+        }
+
+        dText.text = dialogLines[currentLine];
+    }
+
+    public void ShowBox(string dialogue)
+    {
+        dialogActive = true;
+        dBox.SetActive(true);
+        dText.text = dialogue;
+    }
+
+    public void ShowDialogue()
+    {
+        dialogActive = true;
+        dBox.SetActive(true);
+    }
+}
