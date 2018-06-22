cbe87aa2430fe6546d8030525972d25605c9bbb7
Rat <urehelonn@gmail.com>
2018-06-22 03:22:40 -0400

    Auto stash before merge of "master" and "origin/master"
    EatSystem Added

---
+++ Assets/Scripts/Eat/EatSelected.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EatSelected : MonoBehaviour {
+
+    public Button bt;
+    public GameObject eatOption;
+    //public 
+
+	void Start () {
+        bt.onClick.AddListener(triggerEatOption);
+    }
+	
+	void triggerEatOption() {
+        eatOption.SetActive(true);
+	}
+}
