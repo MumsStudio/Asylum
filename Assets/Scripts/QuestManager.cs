967ee69c351342105301807f19b694b88b6224ec
Unknown <asd446902934@qq.com>
2018-06-30 20:01:51 -0400

    Basic quest event

---
+++ Assets/Scripts/QuestManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour {
+    public QuestObject[] quests;
+    public bool[] questCompleted;
+
+    public DMM theDM;
+	// Use this for initialization
+	void Start () {
+        questCompleted = new bool[quests.Length];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void ShowQuestText(string questText)
+    {
+        theDM.dialogLines = new string[1];
+        theDM.dialogLines[0] = questText;
+
+        theDM.currentLine = 0;
+        theDM.ShowDialogue();
+    }
+}
---
+++ Assets/Scripts/QuestObject.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+
+using UnityEngine;
+
+public class QuestObject : MonoBehaviour {
+
+    public int questNumber;
+
+    public QuestManager theQM;
+
+    public string startText;
+    public string endText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void StartQuest()
+    {
+        theQM.ShowQuestText(startText);
+    }
+    public void EndQuest()
+    {
+        theQM.ShowQuestText(endText);
+        theQM.questCompleted[questNumber] = true;
+        gameObject.SetActive(false);
+    }
+}
