f1ca192e4853000a7ca2c34d48f88b462d5cb7d0
Unknown <asd446902934@qq.com>
2018-07-08 19:00:06 -0400

    Location Quest Trigger

---
+++ Assets/Scripts/QuestTrigger.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class QuestTrigger : MonoBehaviour {
+
+    private QuestManager theQm;
+    public int questNumber;
+
+    public bool startQuest;
+    public bool endQuest;
+	// Use this for initialization
+
+	void Start () {
+        theQm = FindObjectOfType<QuestManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            if (!theQm.questCompleted[questNumber])
+            {
+                if (startQuest && !theQm.quests[questNumber].gameObject.activeSelf)
+                {
+                    theQm.quests[questNumber].gameObject.SetActive(true);
+                    theQm.quests[questNumber].StartQuest();
+                }
+                if (endQuest && theQm.quests[questNumber].gameObject.activeSelf)
+                {
+                    theQm.quests[questNumber].EndQuest();
+                }
+            }
+        }
+    }
+}
