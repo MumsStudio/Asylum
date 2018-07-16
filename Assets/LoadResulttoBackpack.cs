5264bd3613283a7f92faf2c97cf6b03e8b6f5b31
madhotdog <jaymjj828@gmail.com>
2018-07-15 22:07:55 -0400

    connect menu and backpack

---
+++ Assets/LoadResulttoBackpack.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadResulttoBackpack : MonoBehaviour {
+
+    public GameObject bagMenu;
+
+    public List<GameObject> items;
+
+    public GameObject backpack;
+
+    void Awake()
+    {
+        bagMenu.SetActive(false);
+    }
+
+    public void LoadThingToBackpack()
+    {
+
+        int size = backpack.GetComponentInChildren<Backpack>().items.Count;
+
+        switch (size)
+        {
+            case 0:
+                items[0].SetActive(false);
+                items[1].SetActive(false);
+                items[2].SetActive(false);
+                break;
+            case 1:
+                items[0].SetActive(true);
+                items[1].SetActive(false);
+                items[2].SetActive(false);
+                break;
+            case 2:
+                items[0].SetActive(true);
+                items[1].SetActive(true);
+                items[2].SetActive(false);
+                break;
+            default:
+                items[0].SetActive(true);
+                items[1].SetActive(true);
+                items[2].SetActive(true);
+                break;
+        }
+
+
+    }
+}
