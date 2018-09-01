13e7db37106f2b35b97e48373111358e003edd94
madhotdog <jaymjj828@gmail.com>
2018-09-01 19:48:26 -0400

    finish the inventory(itemsList)

--- Assets/Scripts/loadbackpacktoItemlist.cs
+++ Assets/Scripts/loadbackpacktoItemlist.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class loadbackpacktoItemlist : MonoBehaviour {
+public class loadbackpacktoItemlist : MonoBehaviour
+{
 
     public GameObject bagMenu;
 
     public List<GameObject> items;
+
+    public List<GameObject> counts;
 
     public List<Text> texts;
 
@@ -15,10 +18,21 @@ public class loadbackpacktoItemlist : MonoBehaviour {
 
     public GameObject backpack;
 
- 
+
     void Awake()
     {
         bagMenu.SetActive(false);
+    }
+
+    public void clear()
+    {
+        int size = backpack.GetComponentInChildren<Backpack>().items.Count;
+
+        for (int i = 0; i < size; i++)
+        {
+            items[i].SetActive(false);
+
+        }
     }
 
     public void LoadThingToBackpack()
@@ -26,48 +40,19 @@ public class loadbackpacktoItemlist : MonoBehaviour {
 
         int size = backpack.GetComponentInChildren<Backpack>().items.Count;
 
-        switch (size)
+        for (int i = 0; i < size; i++)
         {
-            case 0:
-                items[0].SetActive(false);
-                items[1].SetActive(false);
-                items[2].SetActive(false);
-                break;
-            case 1:
-                items[0].SetActive(true);
-                texts[0].GetComponentInChildren<Text>().text = backpack.GetComponentInChildren<Backpack>().items[0].item.name;
+            items[i].SetActive(false);
 
-                items[1].SetActive(false);
-                items[2].SetActive(false);
+        }
 
-                images[0].sprite = backpack.GetComponentInChildren<Backpack>().items[0].item.itemImage;
-                break;
-            case 2:
-                items[0].SetActive(true);
-                texts[0].GetComponentInChildren<Text>().text = backpack.GetComponentInChildren<Backpack>().items[0].item.name;
+        for (int i = 0; i < size; i++)
+        {
+            items[i].SetActive(true);
+            texts[i].GetComponentInChildren<Text>().text = backpack.GetComponentInChildren<Backpack>().items[i].item.name;
+            counts[i].GetComponentInChildren<Text>().text = "X" + "  " + backpack.GetComponentInChildren<Backpack>().items[i].count;
 
-                items[1].SetActive(true);
-                texts[1].GetComponentInChildren<Text>().text = backpack.GetComponentInChildren<Backpack>().items[1].item.name;
-
-                items[2].SetActive(false);
-
-                images[0].sprite = backpack.GetComponentInChildren<Backpack>().items[0].item.itemImage;
-                images[1].sprite = backpack.GetComponentInChildren<Backpack>().items[1].item.itemImage;
-                break;
-            default:
-                items[0].SetActive(true);
-                texts[0].GetComponentInChildren<Text>().text = backpack.GetComponentInChildren<Backpack>().items[0].item.name;
-
-                items[1].SetActive(true);
-                texts[1].GetComponentInChildren<Text>().text = backpack.GetComponentInChildren<Backpack>().items[1].item.name;
-
-                items[2].SetActive(true);
-                texts[2].GetComponentInChildren<Text>().text = backpack.GetComponentInChildren<Backpack>().items[2].item.name;
-
-                images[0].sprite = backpack.GetComponentInChildren<Backpack>().items[0].item.itemImage;
-                images[1].sprite = backpack.GetComponentInChildren<Backpack>().items[1].item.itemImage;
-                images[2].sprite = backpack.GetComponentInChildren<Backpack>().items[2].item.itemImage;
-                break;
+            images[i].sprite = backpack.GetComponentInChildren<Backpack>().items[i].item.itemImage;
         }
 
 
